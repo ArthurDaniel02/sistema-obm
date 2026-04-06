469222bbb13d53346738d7466a6fed6c58ffe930
ArthurDaniel02 <arthurd4niel02@gmail.com>
2026-04-06 14:24:50 -0300

    Commit 5

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', [AgenteController::class, 'index'])->name('agentes.index');
 Route::get('/novo', [AgenteController::class, 'create'])->name('agentes.create');
 Route::post('/agentes', [AgenteController::class, 'store'])->name('agentes.store');
-Route::get('/agentes/{id}/editar', [AgenteController::class,'edit'])->name('agentes.editar');
+Route::get('/agentes/{id}/editar', [AgenteController::class,'edit'])->name('agentes.edit');
 Route::put('/agentes/{id}', [AgenteController::class, 'update'])->name('agentes.update');
 Route::delete('/agentes/{id}', [AgenteController::class, 'destroy'])->name('agentes.destroy');
